e6448f345d6667a558d000d7a48360358627c23b
Praveen Ramachandran <praveen.thayikkattil@gmail.com>
2021-05-21 17:02:17 +0530

    Navbar with dummy pre-router links

--- src/App.js
+++ src/App.js
@@ -7,6 +7,18 @@ function App() {
         <button className="nav-open">MENU</button>
 
         <a className="app-logo" href='/'>Iframe Portal</a>
+
+        <nav id="main-nav" className='sidebar-active'>
+              <button className="nav-close">+</button>
+
+              <h6 className="logo-text">Iframe Portal</h6>
+
+              <ul>
+                <li><a href='/'>Home</a></li>
+                <li><a href='/sign-in'>Sign In</a></li>
+                <li><a href='/sign-up'>Sign Up</a></li>
+              </ul>
+            </nav>
       </div>
     </div>
   );
